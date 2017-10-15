903fe9e75709ffe3dee0c504331449b18ba08abb
Napat Rungruangbangchan <napat.joe@gmail.com>
2017-10-15 18:01:48 +0700

    fixed /music/ url regex and add /music/<album)id>/ url handler

--- music/urls.py
+++ music/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'$', views.index, name="index"),		# default homepage(index)
+	# /music/ ,this is default homepage(index)
+	url(r'^$', views.index, name="index"),		
+
+	# /music/nnn/ ,when nnn represent album_id that is a number ie: 1, 2, 500
+	url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'), 
 ]
--- music/views.py
+++ music/views.py
@@ -3,3 +3,5 @@ from django.http import HttpResponse
 def index(request):
 	return HttpResponse("<h1>This is the music app homepage</h1>")
 
+def detail(request, album_id):
+	return HttpResponse(f"<h2>Details for Album id: {album_id}</h2>")
