f19be9a0d895e291e34d682179b34a72426427f7
Napat Rungruangbangchan <napat.joe@gmail.com>
2017-10-16 18:32:19 +0700

    Basic Django render html Template

--- music/views.py
+++ music/views.py
@@ -1,7 +1,22 @@
-from django.http import HttpResponse
+from django.http import Http404
+#from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Album
 
 def index(request):
-	return HttpResponse("<h1>This is the music app homepage</h1>")
+	all_albums = Album.objects.all()	# Use api to get all object in Album database 
+	return render(request, 'music/index.html', {'all_albums': all_albums,})		
+
+	# all_albums = Album.objects.all()	# Use api to get all object in Album database 
+	# template = loader.get_template('music/index.html')
+	# context = {
+	# 	'all_albums': all_albums,
+	# }
+	# return HttpResponse(template.render(context, request))
 
 def detail(request, album_id):
-	return HttpResponse(f"<h2>Details for Album id: {album_id}</h2>")
+	try:
+		album = Album.objects.get(pk=album_id)
+	except Album.DoesNotExist:
+		raise Http404("Album does not exist")
+	return render(request, 'music/detail.html', {'album': album})	
