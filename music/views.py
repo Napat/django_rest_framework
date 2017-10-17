64835734aaad633aafdb75cb2259e4e0a42e5d2e
Napat Rungruangbangchan <napat.joe@gmail.com>
2017-10-18 00:58:58 +0700

    using get_object_or_404() of django.shortcuts to reduce try/except DoseNotExist

--- music/views.py
+++ music/views.py
@@ -1,12 +1,13 @@
-from django.http import Http404
+#from django.http import Http404
 #from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Album
 
 def index(request):
 	all_albums = Album.objects.all()	# Use api to get all object in Album database 
 	return render(request, 'music/index.html', {'all_albums': all_albums,})		
 
+	#### same as
 	# all_albums = Album.objects.all()	# Use api to get all object in Album database 
 	# template = loader.get_template('music/index.html')
 	# context = {
@@ -15,8 +16,12 @@ def index(request):
 	# return HttpResponse(template.render(context, request))
 
 def detail(request, album_id):
-	try:
-		album = Album.objects.get(pk=album_id)
-	except Album.DoesNotExist:
-		raise Http404("Album does not exist")
+	album = get_object_or_404(Album, pk=album_id)
 	return render(request, 'music/detail.html', {'album': album})	
+
+	#### same as
+	# try:
+	# 	album = Album.objects.get(pk=album_id)
+	# except Album.DoesNotExist:
+	# 	raise Http404("No Album matches the given query")
+	# return render(request, 'music/detail.html', {'album': album})	
