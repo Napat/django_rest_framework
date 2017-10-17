3e49503511c90031f6b0e4c8c4cc52bae13bc7d5
Napat Rungruangbangchan <napat.joe@gmail.com>
2017-10-18 00:53:26 +0700

    Remove hardcode URLs with namespace guard

--- music/urls.py
+++ music/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'music'	# namespace of this urls.py
+
 urlpatterns = [
 	# /music/ ,this is default homepage(index)
 	url(r'^$', views.index, name="index"),		
 
-	# /music/nnn/ ,when nnn represent album_id that is a number ie: 1, 2, 500
+	# /music/<album_id>/ ,when <album_id> represent album_id that is a number ie: 1, 2, 500
 	url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'), 
 ]
