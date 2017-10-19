56128ffc991b3a0abd1fea4a1374df538bab49bd
Napat Rungruangbangchan <napat.joe@gmail.com>
2017-10-20 01:21:38 +0700

    Replace old music function with django generic views

--- music/urls.py
+++ music/urls.py
@@ -3,13 +3,23 @@ from . import views
 
 app_name = 'music'	# namespace of this urls.py
 
+
 urlpatterns = [
-	# /music/ :this is default homepage(index)
-	url(r'^$', views.index, name="index"),		
-
-	# /music/<album_id>/ :when <album_id> represent album_id that is a number ie: 1, 2, 500
-	url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'), 
-
-	# /music/<album_id>/favorite/ 
-	url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'), 
+	url(r'^$', views.IndexView.as_view(), name="index"),		
+	url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'), 
 ]
+
+
+# ###############################################################
+# Deplicate code: Only for understand detail
+# ###############################################################
+# urlpatterns = [
+# 	# /music/ :this is default homepage(index)
+# 	url(r'^$', views.index, name="index"),		
+#
+# 	# /music/<album_id>/ :when <album_id> represent album_id that is a number ie: 1, 2, 500
+# 	url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'), 
+#
+# 	# /music/<album_id>/favorite/ 
+# 	url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'), 
+# ]
--- music/views.py
+++ music/views.py
@@ -1,44 +1,68 @@
-#from django.http import Http404
-#from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
-from .models import Album
+###############################################################
+# Using django generic views
+###############################################################
+from django.views import generic
+from .models import Album 
 
-def index(request):
-	all_albums = Album.objects.all()	# Use api to get all object in Album database 
-	return render(request, 'music/index.html', {'all_albums': all_albums,})		
+# generic.ListView: to view the list of objects
+class IndexView(generic.ListView):
+	template_name = 'music/index.html' # plugin template
+	context_object_name = 'all_albums' # overide default get_queryset return variable name: 
+									   # 'object_list' to 'all_albums' that pass to template_name('music/index.html') 
 
-	#### same as
-	# all_albums = Album.objects.all()	# Use api to get all object in Album database 
-	# template = loader.get_template('music/index.html')
-	# context = {
-	# 	'all_albums': all_albums,
-	# }
-	# return HttpResponse(template.render(context, request))
+	def get_queryset(self):
+		return Album.objects.all()
 
-def detail(request, album_id):
-	album = get_object_or_404(Album, pk=album_id)
-	return render(request, 'music/detail.html', {'album': album})	
+# generic.DetialView: to view details of one object
+class DetailView(generic.DetailView):
+	template_name = 'music/detail.html' # plugin template
+	model = Album 						# set object type for handler  
+ 	
 
-	#### same as
-	# try:
-	# 	album = Album.objects.get(pk=album_id)
-	# except Album.DoesNotExist:
-	# 	raise Http404("No Album matches the given query")
-	# return render(request, 'music/detail.html', {'album': album})	
-
-def favorite(request, album_id):
-	album = get_object_or_404(Album, pk=album_id)
-	try:
-		select_song = album.song_set.get(pk=request.POST['song'])
-	except (KeyError, Song.DoesNotExist):
-		return render(request, 'music/detail.html',
-							{
-								'album': album,
-								'error_message': "You did not select a valid song",
-							}
-						)
-	else:
-		select_song.is_favorite = not select_song.is_favorite	# toggle
-		select_song.save()
-		return render(request, 'music/detail.html', {'album': album})
-
+# ###############################################################
+# Deplicate code: Only for understand detail
+# ###############################################################
+# #from django.http import Http404
+# #from django.http import HttpResponse
+# from django.shortcuts import render, get_object_or_404
+# from .models import Album
+#
+# def index(request):
+# 	all_albums = Album.objects.all()	# Use api to get all object in Album database 
+# 	return render(request, 'music/index.html', {'all_albums': all_albums,})		
+#
+# 	#### same as
+# 	# all_albums = Album.objects.all()	# Use api to get all object in Album database 
+# 	# template = loader.get_template('music/index.html')
+# 	# context = {
+# 	# 	'all_albums': all_albums,
+# 	# }
+# 	# return HttpResponse(template.render(context, request))
+#
+# def detail(request, album_id):
+# 	album = get_object_or_404(Album, pk=album_id)
+# 	return render(request, 'music/detail.html', {'album': album})	
+#
+# 	#### same as
+# 	# try:
+# 	# 	album = Album.objects.get(pk=album_id)
+# 	# except Album.DoesNotExist:
+# 	# 	raise Http404("No Album matches the given query")
+# 	# return render(request, 'music/detail.html', {'album': album})	
+#
+# def favorite(request, album_id):
+# 	album = get_object_or_404(Album, pk=album_id)
+# 	try:
+# 		select_song = album.song_set.get(pk=request.POST['song'])
+# 	except (KeyError, Song.DoesNotExist):
+# 		return render(request, 'music/detail.html',
+# 							{
+# 								'album': album,
+# 								'error_message': "You did not select a valid song",
+# 							}
+# 						)
+# 	else:
+# 		select_song.is_favorite = not select_song.is_favorite	# toggle
+# 		select_song.save()
+# 		return render(request, 'music/detail.html', {'album': album})
+#
